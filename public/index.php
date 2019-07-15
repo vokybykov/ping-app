8414ef7ac5d0c4982841b6d98b579e9c35e4c9e0
Aleksandr Bykov <abykov@solovat.net>
2019-07-15 16:14:58 +0300

    Public folder added

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+	include_once __DIR__.'/../autoload.php';
+	include_once __DIR__.'/../config.php';
+	
+	$routes = include_once __DIR__.'/../routes.php';
+	
+	use App\Core\Route;
+	
+	$requestRoute = trim($_SERVER["PHP_SELF"], '/');
+	
+	if (isset($routes[$requestRoute])) {
+		$route = new Route($routes[$requestRoute]);
+		$route->call();
+	} else {
+		include __DIR__.'/404.php';
+	}
